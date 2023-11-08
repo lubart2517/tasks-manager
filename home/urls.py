9682abe4d718ef3c7c1c8e6b62a27895142bcdf2
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-11-08 11:13:07 +0200

    Added project_create form  to team detail page

--- home/urls.py
+++ home/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("projects/<int:pk>/", views.ProjectDetailView.as_view(), name="project-detail"),
     path("tasks/<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
     path("tasks/<int:pk>/complete", views.task_complete, name="task-complete"),
-    path("teams/<int:pk>/", views.team_detail_view, name="team-detail"),
+    path("teams/<int:pk>/", views.TeamDetailView.as_view(), name="team-detail"),
 ]
 
 app_name = "home"
